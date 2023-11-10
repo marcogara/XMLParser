80d7ffce25aefd77d08e63ac773dce869af9ae13
marcogara <103924910+marcogara@users.noreply.github.com>
2023-11-10 21:58:29 +0100

    update for the whole project functionality

--- XmlParserProject/src/main/java/com/garagna/xmlparserproject/XmlParserProject.java
+++ XmlParserProject/src/main/java/com/garagna/xmlparserproject/XmlParserProject.java
@@ -1,6 +1,7 @@
-
-
 package com.garagna.xmlparserproject;
+
+import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -8,16 +9,47 @@ import org.xml.sax.helpers.DefaultHandler;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.EnumSet;
 
 public class XmlParserProject {
 
     public static void main(String[] args) {
         try {
-            String filePath = "path/to/your/file.xml";
+            String rootPath = "path/to/your/project"; // Set the root path of your project
+            Files.walkFileTree(Paths.get(rootPath), EnumSet.noneOf(FileVisitOption.class), Integer.MAX_VALUE,
+                    new SimpleFileVisitor<Path>() {
+                        @Override
+                        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                            if (file.toString().endsWith(".jsp")) {
+                                parseJspFile(file.toString());
+                            }
+                            return FileVisitResult.CONTINUE;
+                        }
+
+                        @Override
+                        public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                            // Handle the case where file visit failed
+                            return FileVisitResult.CONTINUE;
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void parseJspFile(String filePath) {
+        try {
             SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser saxParser = factory.newSAXParser();
-            MyHandler handler = new MyHandler();
+            MyHandler handler = new MyHandler(filePath);
             saxParser.parse(new File(filePath), handler);
             handler.printResult();
         } catch (Exception e) {
@@ -29,6 +61,10 @@ public class XmlParserProject {
         private int lineNumber = 0;
         private String currentFileName;
         private boolean foundIssue = false;
+
+        public MyHandler(String currentFileName) {
+            this.currentFileName = currentFileName;
+        }
 
         @Override
         public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
@@ -62,5 +98,3 @@ public class XmlParserProject {
         }
     }
 }
-
-
